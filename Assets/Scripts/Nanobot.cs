344c00432e72d37312fe1c32c0c9ca53c91b2d8e
Robert Randolph <robprand@gmail.com>
2022-02-28 12:21:09 -0700

    Added basic movement for nanbot

--- Assets/Scripts/Nanobot.cs
+++ Assets/Scripts/Nanobot.cs
@@ -2,17 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Nanobot : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class Nanobot : MonoBehaviour {
+    // Constants
+    [SerializeField] private float speed = 12f;
+
+    // States
+    private bool isMoving;
+
+    // Temp Data
+    [SerializeField] Vector3 targetPosition;
+
+    void Start() {
+        // Testing
+        MoveCommand(targetPosition);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void FixedUpdate() {
+        if(isMoving) {
+            Move();
+        }
+    }
+
+    // Commands
+    void MoveCommand(Vector3 position) {
+        isMoving = true;
+        targetPosition = position;
+    }
+
+    // Behavior
+    void Move() {
+        // Determine new position
+        Vector3 distanceFromTarget = targetPosition - transform.position;
+        Vector3 directionToTarget = distanceFromTarget.normalized;
+        Vector3 moveAmount = directionToTarget * speed * Time.deltaTime;
+        Vector3 newPosition = transform.position + moveAmount;
+
+        // Determine if nanobot reached target position
+        if (moveAmount.magnitude > distanceFromTarget.magnitude) {
+            // Reached target position
+            newPosition = targetPosition;
+            isMoving = false;
+        }
+
+        transform.position = newPosition;
     }
 }
